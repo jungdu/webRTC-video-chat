f7bebdb32a6b454ce863c1786a8cb401c19213cd
fred <jungdujang@gmail.com>
2021-04-12 06:45:50 +0900

    Update: socket 연결되면 socket id 업데이트

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,6 +1,5 @@
 import { connectSocket } from "./socketConnection"
 import {addSocketHandler} from "./socketHandler"
 
-console.log("Hello World")
 const socket = connectSocket("http://127.0.0.1:8080/");
 addSocketHandler(socket);
--- src/client/socketHandler.ts
+++ src/client/socketHandler.ts
@@ -1,7 +1,10 @@
 import {Socket} from "socket.io-client"
 
 export function addSocketHandler(socket: Socket){
+  let socketId: string = "";
+
   socket.on("connect", () => {
-    console.log("socket connected...");
-  })
+    socketId = socket.id;
+    console.log("socket connected... socketId:", socket.id);
+  });
 }
