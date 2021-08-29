ab1b5f1eb97a32602d71dc3a98074969b955d816
fred <jungdujang@gmail.com>
2021-08-29 19:18:08 +0900

    Fix: 화면이 바로 보이지 않는 문제 수정

--- packages/client/src/components/common/StyledDivFitContain.tsx
+++ packages/client/src/components/common/StyledDivFitContain.tsx
@@ -2,81 +2,88 @@ import React, { RefObject, useEffect, useState } from "react";
 import styled from "@emotion/styled";
 
 interface DivFitContainProps {
-  className?: string;
-  parentRef: RefObject<HTMLElement>;
-  ratio: number;
+	className?: string;
+	parentRef: RefObject<HTMLElement>;
+	ratio: number;
 }
 
-const Self = styled.div<{size: {
-  width: number;
-  height: number;}
+const Self = styled.div<{
+	size: {
+		width: number;
+		height: number;
+	} | null;
 }>`
-  width: ${({size}) => size.width}px;
-  height: ${({size}) => size.height}px;
+	${({ size }) =>
+		size
+			? `
+    width: ${size.width}px;
+    height: ${size.height}px;
+  ` : `display: none;`}
 
-  label: DivFitContain;
+	label: DivFitContain;
 `;
 
-function calculateContainSize(parentRect: {
-  width: number;
-  height: number;
-}, ratio: number){
-  const parentWidth = parentRect.width;
-  const parentHeight = parentRect.height;
-  
-  const heightBasedParentWidth = parentWidth / ratio;
-  if(heightBasedParentWidth > parentHeight){
-    return {
-      width: parentHeight * ratio,
-      height: parentHeight,
-    }
-  }
-  return {
-    width: parentWidth,
-    height: heightBasedParentWidth,
-  }
+function calculateContainSize(
+	parentRect: {
+		width: number;
+		height: number;
+	},
+	ratio: number
+) {
+	const parentWidth = parentRect.width;
+	const parentHeight = parentRect.height;
+
+	const heightBasedParentWidth = parentWidth / ratio;
+	if (heightBasedParentWidth > parentHeight) {
+		return {
+			width: parentHeight * ratio,
+			height: parentHeight,
+		};
+	}
+	return {
+		width: parentWidth,
+		height: heightBasedParentWidth,
+	};
 }
 
 const StyledDivFitContain: React.FC<DivFitContainProps> = ({
-  children,
-  className,
-  parentRef,
-  ratio,
+	children,
+	className,
+	parentRef,
+	ratio,
 }) => {
-  const [size, setSize] = useState<{
-    width: number;
-    height: number;
-  } | null>(null)
+	const [size, setSize] = useState<{
+		width: number;
+		height: number;
+	} | null>(null);
 
-  useEffect(() => {
-    if(parentRef){
-      if(!parentRef.current){
-        throw new Error("No parentRef.current");
-      }
+	useEffect(() => {
+		if (parentRef) {
+			if (!parentRef.current) {
+				throw new Error("No parentRef.current");
+			}
 
-      const parentRect = parentRef.current.getBoundingClientRect();
-      setSize(calculateContainSize(parentRect, ratio));
-      
-      const resizeObserver = new ResizeObserver((entries) => {
-        const parentRect = entries[0].contentRect
-        const nextSize = calculateContainSize(parentRect, ratio);
-        setSize(nextSize)
-      });
-  
-      resizeObserver.observe(parentRef.current);
-      return () => {
-        resizeObserver.disconnect();
-      }
-    }
-  }, [parentRef])
+			const parentRect = parentRef.current.getBoundingClientRect();
+			setSize(calculateContainSize(parentRect, ratio));
 
-  if(!size){
-    return null;
-  }
+			const resizeObserver = new ResizeObserver((entries) => {
+				const parentRect = entries[0].contentRect;
+				const nextSize = calculateContainSize(parentRect, ratio);
+				setSize(nextSize);
+			});
 
-  return <Self className={className}
-    size={size}
-  >{children}</Self>
+			resizeObserver.observe(parentRef.current);
+			return () => {
+				resizeObserver.disconnect();
+			};
+		}
+	}, [parentRef]);
+
+	return (
+		<Self className={className} size={size}>
+			{children}
+		</Self>
+	);
 };
 
 export default StyledDivFitContain;
